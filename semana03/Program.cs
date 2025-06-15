459eb63ccfa1e5144a0099e39a0150c0532d7802
MonicaZ1996 <momazumu1996@gmail.com>
2025-06-15 08:51:15 -0500

    semana03
    semana03

--- semana03/Program.cs
+++ semana03/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RegistroEstudiante
+{
+    class Estudiante
+    {
+        public string Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string[] Telefonos { get; set; }
+
+        public Estudiante(string id, string nombres, string apellidos, string direccion, string[] telefonos)
+        {
+            Id = id;
+            Nombres = nombres;
+            Apellidos = apellidos;
+            Direccion = direccion;
+            Telefonos = telefonos;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Nombres: " + Nombres);
+            Console.WriteLine("Apellidos: " + Apellidos);
+            Console.WriteLine("Dirección: " + Direccion);
+            Console.WriteLine("Teléfonos:");
+            for (int i = 0; i < Telefonos.Length; i++)
+            {
+                Console.WriteLine("- " + Telefonos[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] telefonos = { "0994032403", "0939893835", "0994686794" };
+            Estudiante estudiante = new Estudiante(
+                "0605713742",
+                "Mónica Maricela",
+                "Zuñiga Muyolema",
+                "Barrio José Mancero entre Juan de Dios Martínez y Ángel Martínez",
+                telefonos
+            );
+            Console.WriteLine("=== REGISTRO DEL ESTUDIANTE ===");
+            estudiante.MostrarInformacion();
+        }
+    }
+}
+
