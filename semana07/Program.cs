1bb6d8abd173f1ffb6089c1ba65043b75f75e7de
MonicaZ1996 <momazumu1996@gmail.com>
2025-07-09 22:47:13 -0500

    semana07
    semana07

---
+++ semana07/Program.cs
@@ -0,0 +1,46 @@
+﻿class BalanceoSimbolos
+{
+    public static bool EstaBalanceado(string expresion)
+    {
+        Stack<char> pila = new Stack<char>();
+
+        foreach (char c in expresion)
+        {
+            if (c == '(' || c == '{' || c == '[')
+            {
+                pila.Push(c);
+            }
+            else if (c == ')' || c == '}' || c == ']')
+            {
+                if (pila.Count == 0) return false;
+
+                char top = pila.Pop();
+                if (!Coinciden(top, c))
+                    return false;
+            }
+        }
+
+        return pila.Count == 0;
+    }
+
+    private static bool Coinciden(char abierto, char cerrado)
+    {
+        return (abierto == '(' && cerrado == ')') ||
+               (abierto == '{' && cerrado == '}') ||
+               (abierto == '[' && cerrado == ']');
+    }
+
+    static void Main()
+    {
+        string entrada = "{7 * (8 + 5) * [(9 - 7) + (4 + 11)]}";
+        Console.WriteLine("Expresión: " + entrada);
+
+        if (EstaBalanceado(entrada))
+            Console.WriteLine("Fórmula balanceada.");
+        else
+            Console.WriteLine("Fórmula NO balanceada.");
+    }
+}
+
+
+
